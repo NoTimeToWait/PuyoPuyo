be4e0ad958e6c5eb4b9f8127f2705a2e3348dc3f
NoTimeToWait <wasteman91@gmail.com>
2016-01-05 15:57:18 +0300

    pretty UI

--- GameloftPuyo/src/visuals/FieldView.java
+++ GameloftPuyo/src/visuals/FieldView.java
@@ -1,11 +1,16 @@
 package visuals;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.util.ArrayList;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.EtchedBorder;
 
 import engine.GameContext;
 import engine.NetworkPlayer;
@@ -20,12 +25,13 @@ public class FieldView extends JPanel {
 	private JLabel nextLabel;
 	private JPanel scorePane;
 	private JPanel nextPane;
+	private JPanel fieldPane;
 	private boolean switched=false;
 	
 	public FieldView() {
 		super();
 		animations = new ArrayList<Animation>();
-		this.setLayout(null);
+		/*this.setLayout(null);
 		//fieldPane.setBounds(0, 36, 192, 384);
 		int fieldWidth =  Options.CELL_WIDTH*Options.DEFAULT_FIELD_WIDTH;
 		scorePane = new JPanel();
@@ -48,7 +54,8 @@ public class FieldView extends JPanel {
 		//this.add(fieldPane);
 		this.add(scorePane);
 		this.add(nextPane);
-		/*this.setLayout(new BorderLayout());
+		*/
+		this.setLayout(new BorderLayout());
 		this.setAlignmentY(TOP_ALIGNMENT);
 		
 		fieldPane = getFieldPane();
@@ -73,7 +80,7 @@ public class FieldView extends JPanel {
 			}
 		};
 		nextPane.setPreferredSize(new Dimension(Options.CELL_WIDTH*2+8, Options.CELL_WIDTH*Options.DEFAULT_FIELD_HEIGHT));
-		JLabel nextLabel = new JLabel(Options.getStrings().getNextBtnText()+":");
+		nextLabel = new JLabel(Options.getStrings().getNextBtnText()+":");
 		nextLabel.setVerticalAlignment(JLabel.TOP);
 		nextLabel.setPreferredSize(new Dimension(Options.CELL_WIDTH*2, Options.CELL_WIDTH*3));
 		nextLabel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
@@ -88,7 +95,7 @@ public class FieldView extends JPanel {
 		centerPane.add(fieldPane);
 		centerPane.add(nextPane);
 		this.add(northPane, BorderLayout.NORTH);
-		this.add(centerPane, BorderLayout.CENTER);*/
+		this.add(centerPane, BorderLayout.CENTER);
 	}
 	
 	public static void shift(GameObject obj) {
@@ -127,16 +134,17 @@ public class FieldView extends JPanel {
 		
 		
 	
-	public void paintComponent(Graphics g)
+	public void paint(Graphics g)
 	{
-		g.setColor(Color.GRAY);
+		fieldPane.repaint();
+		/*g.setColor(Color.GRAY);
 		if (switched) {
 			g.clearRect(0, 0, Options.WINDOW_WIDTH, Options.WINDOW_HEIGHT);
 			switched = false;
 		}
 		g.fillRect(0, 0, Options.CELL_WIDTH*Options.DEFAULT_FIELD_WIDTH, Options.CELL_WIDTH*Options.DEFAULT_FIELD_HEIGHT+4);
 		for (Animation anim:animations)
-			anim.animate(g, this);
+			anim.animate(g, this);*/
 	}
 	
 	public void updateScore() {
@@ -145,6 +153,60 @@ public class FieldView extends JPanel {
 		nextLabel.setText(Options.getStrings().getNextBtnText()+":");
 	}
 	
-	
+	private JPanel getFieldPane() {
+		return new JPanel() {
+			public void paint(Graphics g)
+			{
+				g.setColor(Color.GRAY);
+				if (scorePane==null) {
+					g.clearRect(0, 0, this.getWidth(), this.getHeight());
+				}
+				int marginX = this.getWidth()/2-Options.CELL_WIDTH*Options.DEFAULT_FIELD_WIDTH/2;
+				int marginY = Animation.MARGIN;
+				g.fillRect(0, 0, Options.CELL_WIDTH*Options.DEFAULT_FIELD_WIDTH, Options.CELL_WIDTH*Options.DEFAULT_FIELD_HEIGHT);
+				for (Animation anim:animations)
+					anim.animate(g, this);
+				nextPane.repaint();
+				scorePane.repaint();
+				
+				
+				/*Image[] images = Animation.images;
+				drawObjects = GameContext.getPlayer().getGameObjects(true);
+				if (drawObjects==null) return;
+				if ( GameField.cells!=null)
+				for (int i=0; i<6; i++)
+					for (int j=0; j<12; j++) {
+						GameObject obj = GameField.cells[i][j].gameObject;
+						if (obj!=null)
+						switch (obj.getType()) {
+						case Puyo.RED|GameObject.PUYO_TYPE_MASK: 
+							g.drawImage(images[0],400+i*32, j*32, this); break;
+						case Puyo.GREEN|GameObject.PUYO_TYPE_MASK: 
+							g.drawImage(images[1],400+i*32, j*32, this); break;
+						case Puyo.BLUE|GameObject.PUYO_TYPE_MASK: 
+							g.drawImage(images[2],400+i*32, j*32, this); break;
+						case Puyo.YELLOW|GameObject.PUYO_TYPE_MASK: 
+							g.drawImage(images[3],400+i*32, j*32, this); break;
+						default: g.drawImage(images[4],400+i*32, j*32, this);
+					};
+					}
+					
+				/*
+				for (GameObject obj:drawObjects) 
+					switch (obj.getType()) {
+						case Puyo.RED|GameObject.PUYO_TYPE_MASK: 
+							g.drawImage(images[0],obj.getDrawX(), obj.getDrawY(), this); break;
+						case Puyo.GREEN|GameObject.PUYO_TYPE_MASK: 
+							g.drawImage(images[1],obj.getDrawX(), obj.getDrawY(), this); break;
+						case Puyo.BLUE|GameObject.PUYO_TYPE_MASK: 
+							g.drawImage(images[2],obj.getDrawX(), obj.getDrawY(), this); break;
+						case Puyo.YELLOW|GameObject.PUYO_TYPE_MASK: 
+							g.drawImage(images[3],obj.getDrawX(), obj.getDrawY(), this); break;
+						default: g.drawImage(images[4],obj.getDrawX(), obj.getDrawY(), this);
+					};
+				drawObjects = null;*/
+			}
+		};
+	}
 	
 }
